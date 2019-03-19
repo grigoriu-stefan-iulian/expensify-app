e3f344612a0c407055ddca6b4a2cadf51d96747f
grigoriu-stefan-iulian <grigoriu.stefan.iulian@gmail.com>
2019-03-19 20:54:47 +0200

    remove edit expense navlink

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,6 @@ const Header = () => (
         <h1>Expensify</h1>
         <p><NavLink to="/" activeClassName="is-active" exact={true} >Home</NavLink></p>
         <p><NavLink to="/create" activeClassName="is-active">Create Expense</NavLink></p>
-        <p><NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink></p>
     </header>
 )
 
