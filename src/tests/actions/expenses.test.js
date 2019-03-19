32fc2788cfcb124a5598167fc95966fd79fe472a
grigoriu-stefan-iulian <grigoriu.stefan.iulian@gmail.com>
2019-03-19 20:46:16 +0200

    get firebase expenses on start

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,12 +1,22 @@
 import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
-import { startAddExpense, addExpense, editExpense, removeExpense } from '../../actions/expenses'
+import { startSetExpenses, setExpenses, startAddExpense, addExpense, editExpense, removeExpense } from '../../actions/expenses'
 import expenses from '../fixtures/expenses'
 import database from '../../firebase/firebase'
 
 // id: expect.any(String)
 
 const createMockStore = configureMockStore([thunk])
+
+
+beforeEach((done) => {
+    const expensesData = {}
+    expenses.forEach(({ id, description, note, amount, createdAt }) => {
+        expensesData[id] = { description, note, amount, createdAt }
+    })
+    database.ref('expenses').set(expensesData).then(() => done())
+    
+})
 
 test('should setup remove expense action object', () => {
     const action = removeExpense({ id: '123qwe' })
@@ -82,16 +92,23 @@ test('should add expense with defaults to database and store', () => {
         done()
     })
 })
-// test('should setup addExpense action object with default values', () => {
-//     const action = addExpense()
-//     expect(action).toEqual({
-//         type: "ADD_EXPENSE",
-//         expense: {
-//             description: '',
-//             amount: 0,
-//             createdAt: 0,
-//             note: '',
-//             id: expect.any(String)
-//         }
-//     })
-// })
+
+test('should setup setExpenses action object with data', () => {
+    const action = setExpenses(expenses)
+expect(action).toEqual({
+    type: 'SET_EXPENSES',
+    expenses
+})
+})
+
+test('should fetch the expenses from firebase', (done) => {
+    const store = createMockStore({})
+    store.dispatch(startSetExpenses()).then(() => {
+        const actions = store.getActions()
+        expect(actions[0]).toEqual({
+            type: 'SET_EXPENSES',
+            expenses
+        })
+        done()
+    })
+})
