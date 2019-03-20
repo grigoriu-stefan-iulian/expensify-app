e357ce017c116d97792d68df4beeab95266a4297
grigoriu-stefan-iulian <grigoriu.stefan.iulian@gmail.com>
2019-03-20 13:43:03 +0200

    routing changes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import AppRouter from './routers/AppRouter'
+import AppRouter, { history } from './routers/AppRouter'
 import configureStore from './store/configureStore'
 import { setTextFilter } from './actions/filters'
 import { startSetExpenses } from './actions/expenses'
@@ -19,17 +19,28 @@ const jsx = (
     </Provider>
 )
 
+let hasRendered = false
+const renderApp = () => {
+    if(!hasRendered) {
+        ReactDOM.render(jsx, document.getElementById('app'))
+        hasRendered = true
+    }
+}
+
+
 ReactDOM.render(<p>Loading...</p>, document.getElementById('app'))
 
-store.dispatch(startSetExpenses()).then(() => {
-    ReactDOM.render(jsx, document.getElementById('app'))
-})
-
-// on app load check auth status and do something if logged in or logged out
+// on app load and auth state change check auth status and do something if logged in or logged out
 firebase.auth().onAuthStateChanged((user) => {
     if (user) {
-        console.log('logged in')
+        store.dispatch(startSetExpenses()).then(() => {
+            renderApp()
+        })
+        if (history.location.pathname === '/') {
+            history.push('/dashboard')
+        }
     } else {
-        console.log('logged out')
+        renderApp()
+        history.push('/')
     }
 })
