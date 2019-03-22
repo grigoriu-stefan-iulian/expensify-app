bf39c9bd71e9db6f83dbf1b690f45927c2d9c24d
grigoriu-stefan-iulian <grigoriu.stefan.iulian@gmail.com>
2019-03-22 16:35:55 +0200

    debbugging

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -53,7 +53,7 @@ export const editExpense = (id, updates) => ({
 
 export const startEditExpense = (id, updates) => {
     return (dispatch, getState) => {
-        const uid = getState().auth.id
+        const uid = getState().auth.uid
         return database.ref(`users/${uid}/expenses/${id}`)
         .update(updates)
         .then(() => {
